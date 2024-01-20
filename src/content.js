a356504e52cf1c27e276e6f6aba3aeafb2dc947c
Pierrot <faberbastian@gmail.com>
2024-01-20 13:39:42 +0000

    clean assets + begin popup

--- src/content.js
+++ src/content.js
@@ -1,8 +1,11 @@
+// Remove popup
 document.querySelector(".ui-widget-overlay.ui-front").remove();
 document
   .querySelector(
     ".ui-dialog.ui-corner-all.ui-widget.ui-widget-content.ui-front"
   )
   .remove();
+
+// Remove button waiting + Show button download
 document.querySelector("button#dlw").remove();
 document.querySelector("input#dlb").style.display = "block";
